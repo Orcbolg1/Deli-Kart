5eea675167c2c2d1853d7e6b1db95c1d7d40ef03
DKim-appdev <danhkim2003@gmail.com>
2023-11-27 11:25:48 -0500

    updates to the premium toppings class

--- src/main/java/com/pluralsight/deli/PremiumTopping.java
+++ src/main/java/com/pluralsight/deli/PremiumTopping.java
@@ -1,11 +1,54 @@
 package com.pluralsight.deli;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Concrete subclass representing a premium topping
 public class PremiumTopping extends Topping {
+    //List of toppings customer can add
+    private boolean isExtraMeat;
+    private boolean isExtraCheese;
+    private static final List<String> meatToppings = new ArrayList<>();
+    static{
+        meatToppings.add("Steak");
+        meatToppings.add("Ham");
+        meatToppings.add("Salami");
+        meatToppings.add("Roast Beef");
+        meatToppings.add("Chicken");
+        meatToppings.add("Bacon");
+
+    }
+
+    private static final List<String> cheeseToppings = new ArrayList<>();
+    static{
+        cheeseToppings.add("American");
+        cheeseToppings.add("Provolone");
+        cheeseToppings.add("Cheddar");
+        cheeseToppings.add("Swiss");
+    }
+
     // Constructor for a premium topping
     public PremiumTopping(String name) {
         super(name);
         // Additional initialization for premium topping, if needed
+        this.isExtraMeat = false;
+        this.isExtraCheese = false;
+    }
+
+    public boolean isExtraMeat() {
+        return isExtraMeat;
+    }
+
+    public void setExtraMeat(boolean extraMeat) {
+        isExtraMeat = extraMeat;
+    }
+
+    public boolean isExtraCheese() {
+        return isExtraCheese;
+    }
+
+    public void setExtraCheese(boolean extraCheese) {
+        isExtraCheese = extraCheese;
     }
 
     // Concrete implementation of the abstract method from the superclass
@@ -29,7 +72,32 @@ public class PremiumTopping extends Topping {
         if (sandwich.getSize() == 12){
             toppingPrice = 3;
         }
-        return toppingPrice;
+
+        double extraPrice = 0;
+        if (!isExtraMeat){
+            if (sandwich.getSize() == 4){
+                extraPrice += .50;
+            }
+            if (sandwich.getSize() == 8){
+                extraPrice += 1.00;
+            }
+            if (sandwich.getSize() == 12){
+                extraPrice += 1.50;
+            }
+        }
+
+        if (!isExtraCheese){
+            if (sandwich.getSize() == 4){
+                extraPrice += .30;
+            }
+            if (sandwich.getSize() == 8){
+                extraPrice += .60;
+            }
+            if (sandwich.getSize() == 12){
+                extraPrice += .90;
+            }
+        }
+        return toppingPrice + extraPrice;
     }
 
     // Additional methods specific to premium topping, if any
