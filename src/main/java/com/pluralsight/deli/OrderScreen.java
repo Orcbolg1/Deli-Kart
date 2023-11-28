cbf2c86d2f84d1ec59046d20b9489bd603370fb7
JGraham-appdev <jgraham@appdev.yearup.org>
2023-11-28 10:53:22 -0500

    update o screen

--- src/main/java/com/pluralsight/deli/OrderScreen.java
+++ src/main/java/com/pluralsight/deli/OrderScreen.java
@@ -1,76 +1,59 @@
 package com.pluralsight.deli;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class OrderScreen {
-    //Method to add drinks to the order
-    public void addDrinksToOrder () {
-        System.out.println("Added Drinks to your Order");
+    public Sandwich createSandwich() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Build your Sandwich");
         barrier('=');
 
-        //Display the available Drink flavour
-        System.out.println("Available drinks to choose: ");
-        for (String flavour : Drink.getAvailableFlavors()) {
-            System.out.println(flavour);
-            barrier('=');
+        int size = getBreadSize();
+        String bread = getBread();
+        List<String> regularTopping = getRegularToppings();
+        String meat = getMeatTopping();
+        String cheese = getCheeseTopping();
+        boolean extraMeat = isExtraMeat();
+        boolean extraCheese = isExtraCheese();
+        List<String> sides = getSauceChoices();
+
+        // Build the sandwich
+        Sandwich sandwich = new Sandwich(size, bread, regularTopping, false, extraMeat, extraCheese);
+        sandwich.setMeat(meat);
+        sandwich.setCheese(cheese);
+        // Add sauces to the sandwich, assuming the Sandwich class has a method to set sauces
+        sandwich.setSauces(sides);
+
+        // Ask if the user wants to add chips
+        System.out.println("Do you want to add chips to your order? (yes/no): ");
+        String addChipsChoice = scanner.next().toLowerCase();
+        if (addChipsChoice.equals("yes")) {
+            addChipsToOrder();
         }
 
-        // Ask the user to select a chip flavour
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the Drink flavour: ");
-        String selectedFlavour = scanner.nextLine().trim().toLowerCase(); // Convert to lower case
-
-        //check if the selected flavour is valid
-        if (Drink.getAvailableFlavors().stream().anyMatch(flavor -> flavor.equalsIgnoreCase(selectedFlavour))) {
-            // Ask the user to select a drink size
-            System.out.println("Available Drink Sizes: S (Small), M (Medium), L (Large)");
-            System.out.println("Enter your drink size: ");
-            char selectedSize = scanner.next().toUpperCase().charAt(0);
-            barrier('=');
-
-            //Check to see if selected size is valid
-            if (selectedSize == 'S' || selectedSize == 'M' || selectedSize == 'L') {
-                //Create an instance of the Drink class with the selected flavor and size
-                Drink drink = new Drink(selectedSize, selectedFlavour);
-                System.out.println("Drink added to the order: " + drink);
-            } else {
-                System.out.println("Invalid Drink Size. Drink not added to the order.");
-            }
-        } else {
-            System.out.println("Invalid Drink Flavour. Drink not added to the order.");
-        }
-    }
-
-    public static void barrier ( char character){
-        for (int i = 0; i < 30; i++) {
-            System.out.print(character);
-        }
-        System.out.println();//Move to the next line after printing out the characters
-    }
-
-    public void addChipsToOrder() {
-        System.out.println("Added Chips to your Order");
-        barrier('=');
-
-        //Display the available Chip flavour
-        System.out.println("Available chips to choose: ");
-        for (String flavor : Chip.getAvailableFlavors()) {
-            System.out.println(flavor);
-            barrier('=');
+        // Ask if the user wants to add drinks
+        System.out.println("Do you want to add drinks to your order? (yes/no): ");
+        String addDrinksChoice = scanner.next().toLowerCase();
+        if (addDrinksChoice.equals("yes")) {
+            addDrinksToOrder();
         }
 
-        // Ask the user to select a chip flavour
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the chip flavour: ");
-        String selectedFlavour = scanner.nextLine().trim().toLowerCase(); // Convert to lower case
+        // Print out the order details
+        System.out.println("Order Details:");
+        System.out.println("Size: " + size);
+        System.out.println("Bread: " + bread);
+        System.out.println("Regular Toppings: " + regularTopping);
+        System.out.println("Meat: " + meat);
+        System.out.println("Cheese: " + cheese);
+        System.out.println("Extra Meat: " + extraMeat);
+        System.out.println("Extra Cheese: " + extraCheese);
+        System.out.println("Sauces: " + sides);
 
-        //check if the selected flavour is valid
-        if (Chip.getAvailableFlavors().stream().anyMatch(flavor -> flavor.equalsIgnoreCase(selectedFlavour))) {
-
-            //create an instance of the chip class with the selected flavor
-            Chip chip = new Chip(selectedFlavour);
-            System.out.println("Chips added to the order: " + chip);
-        } else {
-            System.out.println("Invalid Chip Flavor. Chips not added to the order.");
-        }
+        return sandwich;
     }
 }
